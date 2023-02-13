70eca4ac87d01f16e73f8b3609f96cd30d09f227
Amin Saqi <aminsaghi@gmail.com>
2023-02-13 17:43:36 +0330

    implementing ApiResult.

--- src/app.py
+++ src/app.py
@@ -2,9 +2,11 @@
 import os
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import Response
 
 from src.service.data_service import DataService
+from src.base.results import ApiResult
 
 
 #%% Service setup.
@@ -23,10 +25,17 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root():    
+    return Response(content=ApiResult(success=True))
 
 
 @app.get("/candles/{exchange}/{symbol}/{timeframe}/{count}")
 async def candles(exchange: str, symbol: str, timeframe: str, count: int):
-    return data_service.get_candles(exchange, symbol, timeframe, count)
+    
+    data_result = data_service.get_candles(exchange, symbol, timeframe, count)
+    api_result = ApiResult(data_result)
+
+    if api_result.success:
+        return Response(content=api_result, status_code=status.HTTP_200_OK)
+    else:
+        return Response(content=api_result, status_code=status.HTTP_400_BAD_REQUEST)
--- src/base/results.py
+++ src/base/results.py
@@ -1,8 +1,11 @@
 
 from typing import TypeVar, Generic
 
+import pandas as pd
+
 
 T = TypeVar('T')
+Dict = TypeVar('Dict', dict)
 
 
 class ServiceResult(Generic[T]):
@@ -12,4 +15,20 @@ class ServiceResult(Generic[T]):
         self.success: bool = success
         self.result: T = result
         self.message: str = message
+
+
+class ApiResult(ServiceResult[Dict]):
+    
+    def __init__(self, service_result: ServiceResult[pd.DataFrame]=None, success: bool = None, result: Dict = None, message: str = None):    
+
+        if service_result:    
+            self.success = service_result.success
+            self.result = service_result.result.to_dict(orient="records") if service_result.result else {}
+            self.message = service_result.message
+        else:
+            self.success = success
+            self.result = result
+            self.message = message            
+
+
         
