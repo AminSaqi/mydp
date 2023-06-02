b2637f1ddc3f40c202090fe91c3fa51833785ba9
Amin Saqi <aminsaghi@gmail.com>
2023-06-02 18:45:46 +0330

    using fastapi startup event.

--- src/app.py
+++ src/app.py
@@ -11,25 +11,22 @@ from src.service.data_service import DataService
 from src.base.results import ApiResult
 
 
-#%% Service setup.
-
-
-with open("config.json", "r") as f:
-    config = json.load(f)    
-
-data_service = DataService(config)
-
-
-#%% FastAPI.
-
-
 app = FastAPI()
+
+
+@app.on_event("startup")
+def setup_data_Service():
+
+    global data_service
+
+    with open("config.json", "r") as f:
+        config = json.load(f) 
+        data_service = DataService(config) 
 
 
 @app.get("/", response_class=JSONResponse)
 async def root():    
     return vars(ApiResult(success=True))
-
 
 @app.get("/candles/{exchange}/{symbol}/{timeframe}/{count}", response_class=JSONResponse)
 async def candles(exchange: str, symbol: str, timeframe: str, count: int, response: Response):
@@ -45,12 +42,12 @@ async def candles(exchange: str, symbol: str, timeframe: str, count: int, respon
 
 @app.websocket("/ws/{exchange}/{symbol}/{timeframe}")
 async def websocket_candles(websocket: WebSocket, exchange: str, symbol: str, timeframe: str):
-
+    print('blah')
     await data_service.connect_websocket(exchange, symbol, timeframe, websocket)  
 
     try:
-        while True:
+        while True:            
             await websocket.receive_text()
             
-    except WebSocketDisconnect:
+    except WebSocketDisconnect:        
         data_service.disconnect_websocket(exchange, symbol, timeframe, websocket)
